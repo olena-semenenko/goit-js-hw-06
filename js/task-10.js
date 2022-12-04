dfe7c31678b3f1bbb687e65db080b2d203469609
olena-semenenko <elen.kinakh@gmail.com>
2022-12-04 14:58:18 +0100

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -21,9 +21,48 @@
 
 // Створи функцію destroyBoxes(), яка очищає вміст div#boxes, у такий спосіб видаляючи всі створені елементи.
 
+const boxesEl = document.querySelector('#boxes');
+const form = document.querySelector('#controls');
+const createBtn = document.querySelector('button[data-create]');
+const destroyBtn = document.querySelector('button[data-destroy]');
+const boxesNumb = form.firstElementChild;
 
+boxesNumb.addEventListener('input', enterNumber);
+createBtn.addEventListener('click', createBoxes);
+destroyBtn.addEventListener('click', deleteBoxes);
+let amount = 0;
 
+function enterNumber() {
+  amount = boxesNumb.valueAsNumber;
+}
 
+function createBoxes() {
+  const markup = [];
+  for (let i = 1; i <= amount; i += 1) {
+    const color = getRandomHexColor();
+    const box = document.createElement('div');
+    const index = 30 + i * 10;
+
+    box.style.width = `${index}px`;
+    console.log(index);
+    box.style.height = `${index}px`;
+    box.style.backgroundColor = `${color}`;
+    console.log(box);
+    markup.push(box);
+  }
+  boxesEl.append(...markup);
+}
+
+function deleteBoxes() {
+  //   console.dir(boxesEl.childNodes
+  // );
+  const boxesCollection = boxesEl.childNodes;
+  boxesCollection.forEach(item => {
+    item.remove();
+  });
+}
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+// console.log(boxesEl.children)
